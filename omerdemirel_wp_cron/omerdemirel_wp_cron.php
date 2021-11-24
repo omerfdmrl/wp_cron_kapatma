fa5ca22f736a33b2d264a388bd5ff41a6fe541c9
Ömer Faruk Demirel <62325045+omerfdmrl@users.noreply.github.com>
2021-11-24 19:27:50 +0300

    Add files via upload

---
+++ omerdemirel_wp_cron/omerdemirel_wp_cron.php
@@ -0,0 +1,15 @@
+
+<?php
+/*
+Plugin Name: OmerDemirel Wp-Cron Kapatma
+Plugin URI:  https://omerdemirel.com.tr/wp-cron-kapatma/
+Description: WooCommerce Wp-Cron Devre Dışı Bırakma
+Version:     1.0
+Author:      Ömer Faruk Demirel
+Author URI:  omerdemirel.com.tr
+License:     GNU
+License URI: https://www.gnu.org/licenses/gpl-3.0.tr.html
+*/
+
+define('DISABLE_WP_CRON', true);
+define( 'ALTERNATE_WP_CRON', false );
